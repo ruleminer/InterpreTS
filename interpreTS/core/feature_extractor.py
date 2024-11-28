76a34166f3d9fbe854b89ce0af6b3e7e73d40f7b
Martyna Kramarz <mk307890@student.polsl.pl>
2024-11-28 09:58:59 +0100

    Update feature_extractor.py

--- interpreTS/core/feature_extractor.py
+++ interpreTS/core/feature_extractor.py
@@ -16,6 +16,7 @@ from .features.feature_entropy import calculate_entropy
 from .features.feature_stability import calculate_stability
 from .features.feature_flat_spots import calculate_flat_spots
 from .features.feature_missing_points import missing_points
+from .features.feature_outliers_std import calculate_outliers_std
 
 class Features:
     LENGTH = 'length'
@@ -33,6 +34,8 @@ class Features:
     CROSSING_POINTS = 'crossing_points'
     MISSING_POINTS = 'missing_points'
     BINARIZE_MEAN = 'binarize_mean'
+    OUTLIERS_STD = 'outliers_std'
+    
 
 class FeatureExtractor:
     def __init__(self, features=None, feature_params=None, window_size=5, stride=1, id_column=None, sort_column=None):
@@ -79,6 +82,7 @@ class FeatureExtractor:
             Features.CROSSING_POINTS: calculate_crossing_points, 
             Features.MISSING_POINTS: missing_points,
             Features.BINARIZE_MEAN: calculate_binarize_mean,
+            Features.OUTLIERS_STD: calculate_outliers_std,
         }
 
     def extract_features(self, data):
